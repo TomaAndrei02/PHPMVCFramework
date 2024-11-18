8eba917184219bfa07dfcbe133589b9435b7b1cf
Toma Andrei <tomaandrey99@gmail.com>
2024-11-18 23:43:45 +0200

    Implement Models

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace app\controllers;
 
 use app\core\Controller;
 use app\core\Request;
+use app\models\RegisterModel;
 
 class AuthController extends Controller{
 
@@ -16,11 +17,23 @@ class AuthController extends Controller{
 
     public function register(Request $request){
 
+        $errors = [];
+        $registerModel = new RegisterModel();
         if($request->isPost()){
-            return 'Handle submitted data!';
+            $registerModel->loadData($request->getBody());
+            
+            if($registerModel->validate() && $registerModel->register()){
+                return 'Succes';
+            }
+
+            return $this->render('register', [
+                'model' => $registerModel
+            ]);
         }
         $this->setLayout('auth');
-        return $this->render('register');
+        return $this->render('register', [
+            'nodel' => $registerModel
+        ]);
 
     }
 }
