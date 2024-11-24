28558d64d01e5628401b495aa403219856aaaf57
Toma Andrei <tomaandrey99@gmail.com>
2024-11-24 18:28:56 +0200

    Restrict signin w same email

--- core/Model.php
+++ core/Model.php
@@ -9,6 +9,7 @@ abstract class Model{
     public const RULE_MIN = 'min';
     public const RULE_MAX = 'max';
     public const RULE_MATCH = 'match';
+    public const RULE_UNIQUE = 'unique';
 
     public function loadData($data)
     {
@@ -65,6 +66,20 @@ abstract class Model{
                     $this->addError($attribute, self::RULE_MATCH, $rule);
                 }
                 
+                if($ruleName === self::RULE_UNIQUE)
+                {
+                    $className = $rule['class'];
+                    $uniqueAttr = $rule['attribute'] ?? $attribute;
+                    $tableName = $className::tableName();
+                    $statement = Application::$app->db->prepare("SELECT * FROM $tableName WHERE $uniqueAttr = :attr");
+                    $statement->bindValue(":attr", $value);
+                    $statement->execute();
+                    $record = $statement->fetchObject();
+                    if($record){
+                        $this->addError($attribute, self::RULE_UNIQUE, ['field' => $attribute]);
+                    }
+                }
+                
             }
         }
 
@@ -89,6 +104,7 @@ abstract class Model{
             self::RULE_MIN => 'Min lenght of this field must be {min}',
             self::RULE_MAX => 'Max lenght of this field must be {max}',
             self::RULE_MATCH => 'This field must be the same as {match}',
+            self::RULE_UNIQUE => 'Record with this {field} already exists',
         ];
     }
 
