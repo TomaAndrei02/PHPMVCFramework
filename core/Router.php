e8d4cc8849825f824a9a8c844de8fffac59657ec
Toma Andrei <tomaandrey99@gmail.com>
2024-11-29 15:01:39 +0200

    Implement Page Title

--- core/Router.php
+++ core/Router.php
@@ -11,8 +11,8 @@ use app\core\exception\NotFoundException;
      *@param \app\core\Response $response
     */
 
-class Router{
-
+class Router
+{
     public Request $request;
     public Response $response;
     protected array $routes = [];
@@ -48,7 +48,7 @@ class Router{
 
         if (is_string($callback))
         {
-            return $this->renderView($callback);
+            return Application::$app->view->renderView($callback);
         }
 
         if (is_array($callback)) 
@@ -69,45 +69,4 @@ class Router{
 
         return call_user_func($callback, $this->request, $this->response);
     }
-
-    public function renderView($view, $params = []){
-
-        $layoutContent = $this->layoutContent();
-        $viewContent = $this->renderOnlyView($view, $params);
-        return str_replace('{{content}}', $viewContent, $layoutContent);
-
-    }
-
-    public function renderContent($viewContent){
-
-        $layoutContent = $this->layoutContent();
-        return str_replace('{{content}}', $viewContent, $layoutContent);
-
-    }
-
-    protected function layoutContent()
-    {
-        $layout = Application::$app->layout;
-        if(Application::$app->controller)
-        {
-        $layout = Application::$app->controller->layout;
-        }
-        ob_start();
-        include_once Application::$ROOT_DIR."/views/layouts/$layout.php";
-        return ob_get_clean();
-
-    }
-
-    protected function renderOnlyView($view, $params){
-        foreach($params as $key => $value)
-        {
-            $$key = $value;
-        }
-
-        ob_start();
-        include_once Application::$ROOT_DIR."/views/$view.php";
-        return ob_get_clean();
-
-    }
-    
 }
