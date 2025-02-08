b380658ec2e8e6cf8fff30c90e4e6ac0b7ad19f0
Toma Andrei <tomaandrey99@gmail.com>
2025-02-08 19:07:34 +0200

    Created a link to React user list

--- views/home.php
+++ views/home.php
@@ -10,9 +10,9 @@ use app\core\Application;
 <?php if(Application::isGuest()): ?>
 
     <h3>Welcome</h3>
-
+    <a href="http://localhost:3000/">Back to list of users</a>
 <?php else: ?>
 
     <h3>Welcome <?php echo Application::$app->user->getDisplayName() ?></h3>
-    
+    <a href="http://localhost:3000/">Back to list of users</a>
 <?php endif; ?>
